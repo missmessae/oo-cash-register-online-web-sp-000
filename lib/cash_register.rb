415b132bffb59ae4eb622417b85fbf3cc25df341
Annie Wiegel <annewiegel17@gmail.com>
2021-02-11 23:18:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
   attr_accessor :cash_register :total :cash_register_with_discount
 
   def initialize(cash_register)
-    :cash_register = cash_register
+    @cash_register = cash_register
     @total = 0
   end
 
