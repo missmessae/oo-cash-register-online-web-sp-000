b7e3471eb8e5681be959ec665966fbedcf93ad49
Annie Wiegel <annewiegel17@gmail.com>
2021-02-11 23:24:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,18 +1,9 @@
 class CashRegister
   attr_accessor :cash_register, :total, :cash_register_with_discount
 
-  def initialize(total)
+  def initialize
     @total = 0
   end
-
-  def discount
-    @cash_register_with_discount = CashRegister.new
-
-
-  end
-  let(:cash_register) { CashRegister.new }
-  let(:cash_register_with_discount) { CashRegister.new(20) }
-
 
 
 end
