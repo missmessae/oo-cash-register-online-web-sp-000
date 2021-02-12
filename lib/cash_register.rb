4999646ad1b437667959e6d307fadd12fcf6b9ef
Annie Wiegel <annewiegel17@gmail.com>
2021-02-12 00:06:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,7 +22,7 @@ class CashRegister
 
   def apply_discount
     if @discount > 0
-      @total*@discount
+      @total = (@total * @discount)/100
       puts "After the discount, the total comes to #{self.total}."
     else
       puts "here is no discount to apply."
